63a27f420cc7a6ec208094fe662f981442d5dc1d
Graham Coster <graham.coster@artprocessors.net>
2023-03-31 00:47:05 +1100

    Add in SQLALchemy debug level logging into the logging stored in tmp/debug.log

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import dotenv
 from sqlalchemy_play.play import Play
 
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)  # Add in SQLAlchemy with my overall logging
 LOGGER = logging.getLogger(__name__)
 
 dotenv.load_dotenv(override=True)  # load variables stored in .env, overwriting any system defined variables
 
 
 def main():
-    player = Play()
-    player.log_stuff()
-    player.say("Logging is configured. Yay!")
+    play = Play()
+    play.run_all()
 
 
 if __name__ == "__main__":
